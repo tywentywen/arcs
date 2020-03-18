16476e53ec2e119944a2ff8fa049e63a66bdbd5b
Tywen Kelly <Tywen@Tywens-Second-MacBook-Pro.local>
2020-03-17 20:06:28 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //strokeCap(ROUND)
+}
+
+
+function draw() {
+  background(255);
+  stroke(0);
+  noFill();
+
+  mX = map(mouseX, 0, windowWidth, 20, 200)
+  mY = map(mouseY, 0, windowHeight, 20, 200)
+
+  makeArc(mX, mY, ((mX + mY)/4));
+}
+
+
+function makeArc(mX, mY, amp){
+  //mX and mY are grid and arc size
+  time = millis() / 300;
+
+  for (let y = 0; y < windowHeight; y+= mY){
+    sw = abs(amp * sin(y * amp + time));
+    for (let x1 = 0; x1 < windowWidth; x1 += 2 * mX){
+      strokeWeight(sw*(mY/40));
+      stroke(255,30,50)
+      arc(x1, y, mX, mY, 0, PI);
+    }
+
+    for (let x2 = 0; x2 < windowWidth; x2 += 2 * mX){
+      strokeWeight(sw*(mX/40));
+      stroke(255,30,50);
+      arc(x2 + mX, y, mX, mY, PI, 2 * PI);
+    }
+  }
+}
